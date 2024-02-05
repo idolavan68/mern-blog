d5055f167672891a9328755c91ff134d8bc04e41
Ido Lavan <idolavan68m>
2024-02-05 22:07:44 +0200

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,17 @@
 import express from 'express'
+import mongoose from 'mongoose'
+import 'dotenv/config'
 const app = express()
 const port = 3000
+
+
+
+mongoose.connect(process.env.MONGO_URL)
+    .then(console.log("connecting to Mongo DB")
+    ).catch((err) => console.log(err))
+
+
+
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
